625e5d62075fc4d52d7142baab6f12845992bd36
Sheila O'Donnell <sheila@contexttravel.com>
2020-03-26 14:49:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function greet(timeString){
     else if (timeHour >= 12) {
       return 'Good Afternoon'
     }
-    else if (timeHour > 5 && < 12) {
+    else if (timeHour > 5 && timeHour < 12) {
       return 'Good Evening'
     }
   
