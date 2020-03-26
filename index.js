96d8f6995ece68447cf14aaee09300d37c63cb16
Sheila O'Donnell <sheila@contexttravel.com>
2020-03-26 14:53:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-/* Given Code, don't edit */
-
 function handleClick(e) {
   const timeString = document.getElementById('time').value
   displayMessage(greet(timeString))
 }
 
-/* End Given Code, don't edit above here...*/
+function greet(timeString){
+  let time = timeString.split(':')
+  let timeHour = parseInt(time[0])
+    if (timeHour < 12) {
+    return 'Good Morning';
+  }
+    else if (timeHour >= 12 && timeHour <= 17) {
+      return 'Good Afternoon'
+    }
+    else if (timeHour > 17) {
+      return 'Good Evening'
+    }
+}
 
-/* Write your implementation of greet() */
-/* Write your implementation of displayMessage() */
+function displayMessage(bestGreeting){
+  document.getElementById('greeting').innerText = bestGreeting
+}
+
