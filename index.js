df234ac54cdb34d1a76fd6f04d8ac950252c805e
Sheila O'Donnell <sheila@contexttravel.com>
2020-03-26 14:47:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,11 +9,10 @@ function greet(timeString){
   let time = timeString.split(':')
   let timeHour = parseInt(time[0])
     if (timeHour < 12) {
-    return 'Good Morning'
-  
+    return 'Good Morning';
+  }
     elsif (timeHour >= 12) {
       return 'Good Afternoon'
-    }
     }
   
   
